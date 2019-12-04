3dae9f4119ecb7dbb098a515d302815b34edf9c6
wolel <56022602+wolel@users.noreply.github.com>
2019-12-04 13:11:31 +0100

    revision tableaux

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
 Consignes :
 
-- Creer ( en javascript ou en html ) un champ texte et proposer à l'utilisateur d'entrer une valeur, créer un bouton
+- Creer (en javascript ou en html ) un champ texte et proposer à l'utilisateur d'entrer une valeur, créer un bouton
 et écouter le click sur ce bouton
-- Récuperer la valeur entré par l'utilisateur lors du click sur le bouton
-- Stocker cette valeur dans un tableau
-- Lorsque l'utilisateur a entré au moins 10 valeurs, afficher dans un élément div une entrée du tableau
-choisie au hasard
-- Dans un autre div, afficher à l'utilisateur la 5 eme entrée du tableau
-- Tant que l'utilisateur n'a pas entré au moins 10 valeurs, afficher dans l'élément div le message suivant
+1) Récuperer la valeur entré par l'utilisateur lors du click sur le bouton
+2) Stocker cette valeur dans un tableau
+3) Lorsque l'utilisateur a entré au moins 10 valeurs, afficher dans un élément div une entrée du tableau
+    choisie au hasard
+4) Dans un autre div, afficher à l'utilisateur la 5 eme entrée du tableau
+5) Tant que l'utilisateur n'a pas entré au moins 10 valeurs, afficher dans l'élément div le message suivant
 "entrez au moins 10 valeurs"
-- Ajouter un bouton permettant à l'utilisateur d'afficher une entrée aléatoire du tableau.
-- Ajouter un bouton permettant à l'utilisateur d'afficher l'intégralité du tableau ( les index de chaque éléments suivi
+6) Ajouter un bouton permettant à l'utilisateur d'afficher une entrée aléatoire du tableau.
+7) Ajouter un bouton permettant à l'utilisateur d'afficher l'intégralité du tableau ( les index de chaque éléments suivi
 d'un - et de la valeur de chaque élément )
-- Ajouter un bouton permettant de supprimer le dernier élément du tableau
+8) Ajouter un bouton permettant de supprimer le dernier élément du tableau
 - Ajouter un bouton permettant de supprimer tout les éléments du tableau
 
 
---
+++ exercice1.js
@@ -0,0 +1,105 @@
+var btnAdd = document.getElementById('btnAdd');
+var btn5 = document.getElementById('btn5');
+var btn3 = document.getElementById('btn3');
+var btnDel = document.getElementById('btnDel');
+var btnDelAll = document.getElementById('btnDelAll');
+var btnAleatoire = document.getElementById('btnAleatoire');
+
+var txt =  document.getElementById('txt');
+
+
+
+
+/**
+ * 1) Récuperer la valeur entré par l'utilisateur lors du click sur le bouton
+ */
+btnAdd.addEventListener('click', function () {
+    tab.push(txt.value);
+    tabField.innerHTML = tab;
+    //console.log(tabField);
+});
+/**
+ * 2) Stocker cette valeur dans un tableau
+ */
+var tab = [];
+var tabField = document.getElementById('tabField');
+//tabField.length = txt.value.length;
+
+
+/**
+ * 3) Lorsque l'utilisateur a entré au moins 10 valeurs, afficher dans un élément div une entrée du tableau
+ choisie au hasard
+ */
+//c'est deja fait avec la div 'tabField
+
+/**
+ * 4) Dans un autre div, afficher à l'utilisateur la 5 eme entrée du tableau
+ */
+
+var entreNr5 = document.createElement('div');
+document.getElementById('tabField5').appendChild(entreNr5);
+entreNr5.innerHTML.toUpperCase();
+
+
+btn5.addEventListener("click", function () {
+    entreNr5.innerHTML  = tab[4];
+
+    if (tab.length <= 3){
+        alert('entrez au moins 5 valeurs');
+        console.log(tabField);
+
+    }
+    else{
+        //alert('votre 5 éme element est ' + entreNr5.innerHTML.toUpperCase() );
+    }
+
+});
+
+btn3.addEventListener("click", function () {
+   entreNr5.innerHTML = tab[2];
+    //alert('votre 5 éme element est ' + entreNr5.innerHTML.toUpperCase() )
+
+});
+
+/**
+ * 5) Tant que l'utilisateur n'a pas entré au moins 10 valeurs, afficher dans l'élément div le message suivant
+ "entrez au moins 10 valeurs"
+ */
+
+//deja fait avec la condition if (tab.lenght <= 4)
+
+/**
+* 6) Ajouter un bouton permettant à l'utilisateur d'afficher une entrée aléatoire du tableau.
+*/
+
+var mots_aleatoire;
+var random;
+
+btnAleatoire.addEventListener("click", function () {
+    random = Math.floor(Math.random()*tab.length);
+
+
+    for (var i = 0; i < tab.length; i++ ){
+        mots_aleatoire = tab[random];
+      entreNr5.innerHTML = tab[random];
+    }
+});
+/**
+ 8) Ajouter un bouton permettant de supprimer le dernier élément du tableau
+ */
+
+btnDel.addEventListener('click',function () {
+    tab.pop(txt.value);
+    tabField.innerHTML = tab;
+    console.log(tabField)
+});
+
+/**
+ 9) Ajouter un bouton permettant de supprimer tout les éléments du tableau
+ */
+
+btnDelAll.addEventListener('click',function () {
+    tab.splice(txt.value);
+    tabField.innerHTML = tab;
+    //console.log(tabField)
+});
